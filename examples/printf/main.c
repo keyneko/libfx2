bac6cf34daa4e6e6a6f0557109f47f38a18a4e23
周翔 <keyneko@163.com>
2024-04-28 02:07:50 -0700

    feat: add install cmds, and modify the printf example

--- examples/printf/main.c
+++ examples/printf/main.c
@@ -1,18 +1,24 @@
 #include <fx2regs.h>
 #include <fx2debug.h>
+#include <fx2delay.h>
 #include <stdio.h>
 
-DEFINE_DEBUG_PUTCHAR_FN(PA0, 57600)
+DEFINE_DEBUG_PUTCHAR_FN(PA0, 9600)
 
 int main(void) {
   // Any of these will work at 57600 baud:
-  CPUCS = 0;
-  // CPUCS = _CLKSPD0;
-  // CPUCS = _CLKSPD1;
+  // CPUCS = 0;  // 12MHz
+  CPUCS = _CLKSPD0;  // 24MHz
+  // CPUCS = _CLKSPD1; // 48Mhz
 
   OEA = (1U<<0);
   PA0 = 1;
 
-  printf("Hello, world!\n");
-  while(1);
+  int cnt = 0;
+
+  while(1) {
+    delay_ms(1000);
+    printf("Hello, world %d\r\n", cnt);
+    cnt++;
+  };
 }
